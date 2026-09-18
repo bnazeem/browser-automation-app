84ff24a740b74ebc908703e9d159d3a7c415c54a
Nazeem Bahadoor <bnazeem@gmail.com>
2026-09-17 21:39:08 -0400

    feat: add clerk orgs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { auth } from "@clerk/nextjs/server"
 
 export default async function Page() {
   await auth.protect()
 
-  return <UserButton />
+  return (
+    <div className="flex flex-col items-start gap-4 p-6">
+      <UserButton />
+      <OrganizationSwitcher hidePersonal />
+    </div>
+  )
 }
